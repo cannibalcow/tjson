837f654f62c9e45b79523dd9a329bc69f4ed2881
Daniel Heldt <daheld@lm.se>
2023-11-23 08:20:38 +0100

    more args

--- src/args.rs
+++ src/args.rs
@@ -4,10 +4,18 @@ pub mod args {
     #[derive(Parser, Debug)]
     #[command(author, version, about, long_about = None)]
     pub struct TJsonArgs {
-        #[arg(short, long)]
+        #[arg(short, long, help = "Path to in json struct. Eg, /path/to/json/node")]
         pub pointers: Vec<String>,
 
-        #[arg(short, long)]
+        #[arg(short, long, help = "Http json resource, if not given read stdin")]
         pub source: String,
+
+        #[arg(
+            short = 'i',
+            long = "polling-intervall",
+            default_value_t = 3,
+            help = "Polling interval in seconds"
+        )]
+        pub polling_interval: usize,
     }
 }
